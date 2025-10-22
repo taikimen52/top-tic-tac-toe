8b2691e0f0d4f53a3619216744fab2f6d2d7a76b
Taikimen52 <5taikiiida@gmail.com>
2025-10-22 18:03:27 +0900

    Editing script.js(Each factory functions)

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
-function Gameboard() {
+function GameBoard() {
     const row = 3;
-    const column = 3;
+    const column = row;
     const board = [];
 
-    for(let i = 0; i < row; i++){
-        board[i] = [];
-        for(let j = 0; j < column; j++){
-            board[i].push(Cell());
+    const initBoard = () => {
+        for(let i = 0; i < row * column; i++){
+            board.push("0");
         }
-    }
-    
-    const getBoard = () => board;
+        return board;
+    };
 
-    return {getBoard}
+    const getBoard = () => board
+
+    return {initBoard, getBoard}
 }
 
-function Cell(){
-    let token = 0;
-}
 
-function GameController(playerOneName = "player 1", playerTwoName = "player 2"){
-
-    // 盤面を作成
-    const board = Gameboard();
-
-    // プレイヤーを配列に入れ、どちらがアクティブか判断
+function setPlayer(playerOneName = "player 1", playerTwoName ="player 2"){
     const players = [
         {
             name: playerOneName,
-            token: 1
+            value: 1
         },
         {
             name: playerTwoName,
-            token: 2
+            value: 2
         }
     ];
-    let activePlayer = players[0];
+
+    const getPlayers = () => players;
+
+    const getActivePlayer = () => players[0];
+
+    return {getPlayers, getActivePlayer}
+}
+
+function GameController(playerOneName, playerTwoName){
+
+    // 盤面とプレイヤーを作成
+    const board = GameBoard();
+    const createPlayers = setPlayer(playerOneName, playerTwoName);
+    const players = createPlayers.getPlayers();
+
+    //初期専攻プレイヤーを取得
+    let activePlayer = createPlayers.getActivePlayer();
+
+
+    const startNewGame = () => {
+        console.log(`Start New game with ${players[0].name}`)
+        board.initBoard()
+    }
+        console.log(board.getBoard())
 
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
 
+    // const setValueToBoard = (boardPosition) => {
+    // }
 
+
+    return {startNewGame, switchPlayerTurn}
 }
+
+const game = GameController("A", "B");
+game.startNewGame();
